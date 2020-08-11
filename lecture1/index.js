b7478884c64d079b9d8fe803564a495f8f43fbbf
mint <joyuiyeong@mintui-MacBookPro.local>
2020-08-12 02:12:04 +0900

    Setup node and express

---
+++ lecture1/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 5943
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Server is listening on ${port}`))

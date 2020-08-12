c6364029d2269300fcad237b61bef8685ad11c4f
yuiyeong <joyuiyeong@gmail.com>
2020-08-12 23:12:26 +0900

    Add mongoose and mongoDB

--- lecture1/index.js
+++ lecture1/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 5943
+
+mongoose.connect('', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false  
+})
+.then(() => console.log('connected to mongo db'))
+.catch(e => console.error(e))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 

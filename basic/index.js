3f045920cb794392ec9ad0d20c9bd37aecccecf7
yuiyeong <joyuiyeong@gmail.com>
2020-08-13 00:03:52 +0900

    Impelement restration

--- basic/index.js
+++ basic/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 
-
 const app = express()
-const port = 5943
+const port = 3000
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// application/json
+app.use(bodyParser.json())
 
 mongoose.connect('', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false  
@@ -18,7 +22,12 @@ mongoose.connect('', {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/register', (req, res) => {
-
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.status(400).json({ success: false, err})
+    
+    return res.status(200).json({ success: true })
+  })
 })
 
 
